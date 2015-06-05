6d4d9804bf0d6cd977a5cb6e2cb80dd00a9350ed
Yanli Xu <v-yanlxu@microsoft.com>
2015-06-05 13:39:31 +0800

    update tests for Object-Assign

--- test/built-ins/Object/Assign/LengthOfAssign.js
+++
@@ -1,7 +0,0 @@
-/*---
-description: The length property of the assign method should be 2
-es6id:  19.1.2.1
-author: Wei Guo
----*/
-
-assert(Object.assign.length === 2 , "The length property of the assign method should be 2");
